322e5c93e9b215e1c7b86033ab3d30ef928df1e3
Portinho <m.porto25@gmail.com>
2026-06-30 22:48:07 -0300

    2268

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers/Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((store) => store.customer.fullName);
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
+      {fullName === "" ? <CreateCustomer /> : <h2>👋 Welcome, {fullName}</h2>}
       <Customer />
       <AccountOperations />
       <BalanceDisplay />
